018c4594009d33bd894b242ee044c7b181ecfb3f
matthewjhough <matthough1@icloud.com>
2018-08-21 21:34:58 -0400

    setup material-ui with typescript & icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,77 @@
-import * as React from 'react';
-import './App.css';
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { Menu as MenuIcon } from "@material-ui/icons";
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+const menuButton: string = "menu-button";
+const flex: string = "flex";
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
+  public state: any = {
+    anchorEl: null,
+    auth: true
+  };
+
   public render() {
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={menuButton}
+              color="inherit"
+              onClick={this.handleMenu}
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={flex}>
+              Skaioskit
+            </Typography>
+            {auth && (
+              <div>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    horizontal: "right",
+                    vertical: "top"
+                  }}
+                  transformOrigin={{
+                    horizontal: "right",
+                    vertical: "top"
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
     );
   }
+
+  private handleMenu = (event: any) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  private handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
 }
 
 export default App;
