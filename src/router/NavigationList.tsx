14788da80895b0c58c14ba225ad891607a41fedd
matthewjhough <matthough1@icloud.com>
2018-08-31 20:09:56 -0400

    global route rename

--- src/router/NavigationList.tsx
+++ src/router/NavigationList.tsx
@@ -25,7 +25,7 @@ export const NavigationList = () => (
         <ListItemIcon>
           <BusinessIcon />
         </ListItemIcon>
-        <ListItemText primary="Operations" />
+        <ListItemText primary="Manage" />
       </ListItem>
     </NavLink>
 
