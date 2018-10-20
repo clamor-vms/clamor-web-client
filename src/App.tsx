4106cd8bcf3c36389bcffd45e181e0f28f9b75a7
Nathan Mentley <nathanmentley@gmail.com>
2018-10-20 17:52:43 -0400

    testing around hitting the services.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,14 @@ import { Styles } from "./AppStyles";
 import { IProps } from "./App.interface";
 import "./App.css";
 
+import { AuthContext, CampaignAboutService } from 'skaioskit-javascript-sdk';
+
+const authContext = new AuthContext(
+    "http://172.17.0.8",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJ1c2VyX2lkIjoxMn0.4VNZyedR2gTlXIR6Rwyn4VorSDhsgIXqZkCgehwpzf4"
+);
+const campaignAboutService = new CampaignAboutService(authContext);
+
 class App extends React.Component<any, any> {
   public authService = new AuthService();
   public state: any = {
@@ -27,6 +35,11 @@ class App extends React.Component<any, any> {
           <p>Redirecting to the authentication service...</p>
         </div>
       );
+    } else {
+        campaignAboutService.GetCampaignServiceInfo()
+            .then((x) => {
+                console.log(x);
+            });
     }
 
     return (
